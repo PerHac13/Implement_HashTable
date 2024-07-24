807b2ed323f36f3b94aadd48ee4703bf5a163afd
perhac <abdullah13112004@gmail.com>
2024-07-24 19:33:40 +0530

    feat: hash function

--- src/hash_table.c
+++ src/hash_table.c
@@ -42,3 +42,21 @@ void ht_del_table(hash_table* ht) {
     free(ht);
 }
 
+// hash function
+static int ht_hash(const char* key, const int prime, const int num_buckets) {
+    long hash = 0;
+    const int len_s = strlen(key);
+    for(int i = 0; i < len_s; i++) {
+        hash += (long)pow(prime, len_s - (i+1)) * key[i];
+        hash = hash % num_buckets;
+    }
+    return (int)hash;
+}
+
+// double hashing function to avoid collisions
+static int ht_get_hash(const char* key, const int num_buckets, const int attempt) {
+    const int hash_a = ht_hash(key, 151, num_buckets);
+    const int hash_b = ht_hash(key, 163, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
